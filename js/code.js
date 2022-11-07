f482d9dcf4de4aab04458cd35e9d3e3bb80ee566
Mu-een <mueenslam@gmail.com>
2022-11-07 15:32:42 +0200

    js

---
+++ js/code.js
@@ -0,0 +1,11 @@
+// Write to a document
+document.write("Hello World");
+document.write("<br>My name is Mu-een Slamat");
+document.write("<br>Am a developer");
+
+
+let userName = "Mu-een";
+let userAge = 23;
+console.log(userName);
+console.log(userAge)
+console.log(typeof userName);
